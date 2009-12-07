4f2295dcbf54d7046f495e7e86d8edddbd068b1c
zigen <zigen@2f4f0dfe-323b-0410-b955-da024bdfb0c0>
2009-12-07 09:23:27 +0000

    git-svn-id: http://svn.sourceforge.jp/svnroot/dbviewer/DBViewer/trunk@140 2f4f0dfe-323b-0410-b955-da024bdfb0c0

--- src/zigen/plugin/db/core/rule/oracle/OracleSQLCreatorFactory.java
+++ src/zigen/plugin/db/core/rule/oracle/OracleSQLCreatorFactory.java
@@ -83,10 +83,14 @@ public class OracleSQLCreatorFactory extends DefaultSQLCreatorFactory {
 		for (int i = 0; i < cols.length; i++) {
 			Column column = cols[i];
 			if (i == 0) {
+				sb.append("\"");
 				sb.append(column.getName());
+				sb.append("\"");
 			} else {
 				sb.append(", ");
+				sb.append("\"");
 				sb.append(column.getName());
+				sb.append("\"");
 			}
 		}
 	}
@@ -97,8 +101,8 @@ public class OracleSQLCreatorFactory extends DefaultSQLCreatorFactory {
 
 	public String createSelectForPager(String _condition, int offset, int limit) {
 		StringBuffer sb = new StringBuffer();
-		sb.append("SELECT ");
-		addColumnName(sb);
+		sb.append("SELECT");
+		addColumnName(sb);	// remove ROWNUM
 		sb.append(" FROM (SELECT W.*, ROWNUM LINE FROM ("); //$NON-NLS-1$
 
 		sb.append(" SELECT * FROM "); //$NON-NLS-1$
